031784336c3ff9450b5794fb906c337abe3a5b01
Jakob Peters <jakobj.peters@gmail.com>
2023-07-29 19:02:49 -0400

    Remove unused code

--- src/Typstry.jl
+++ src/Typstry.jl
@@ -30,34 +30,3 @@ end
 export typst, compile, watch, fonts, @T_str
 
 end # module Typst
-
-
-
-
-#=
-
-# function pdf(path; delete = false)
-#     compile(path)
-#     delete && rm(path)
-#     nothing
-# end
-
-# function pdf(s, path = "out.typ"; delete = false)
-#     write(path, s)
-#     pdf(path; delete)
-# end
-
-# compile(doc, name = "out.pdf")
-
-pdf(s, delete = false, open = true)
-pdf(s::TypstString; path = out.pdf, delete = false, open = true)
-
-
-watch -> use threads?
-
-
-mutable struct X <: AbstractString
-    s::String
-end
-
-=#
