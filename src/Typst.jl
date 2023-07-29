a88545a05ad59c2a0d656e0f0950269627032a62
Alain Marcotte <nialamarcotte@gmail.com>
2023-07-29 13:52:06 -0400

    Implement @T _str

--- src/Typst.jl
+++ src/Typst.jl
@@ -13,11 +13,15 @@ function show(io::IO, ts::TypstString)
     show(io, ts.s)
 end
 
+macro T_str(s)
+    TypstString(s)
+end
+
 ncodeunits(t::TypstString) = ncodeunits(t.s)
 iterate(t::TypstString) = iterate(t.s)
 iterate(t::TypstString, state::Integer) = iterate(t.s, state)
 isvalid(t::TypstString, i::Integer) = Base.isvalid(t.s, i)
 
-export TypstString
+export TypstString, @T_str
 
 end # module Typst
