49869346638ea56286450e3114b4fc7cb69bc39d
Jakob Peters <jakobj.peters@gmail.com>
2023-07-29 11:17:08 -0400

    Implement the iterator interface for `TypstString`

--- src/Typst.jl
+++ src/Typst.jl
@@ -1,6 +1,23 @@
 
 module Typst
 
+import Base: show, ncodeunits, iterate, getindex, isvalid
 using Typst_jll
+
+struct TypstString <: AbstractString
+    s::String
+end
+
+function show(io::IO, ts::TypstString)
+    print(io, "T")
+    show(io, ts.s)
+end
+
+ncodeunits(t::TypstString) = ncodeunits(t.s)
+iterate(t::TypstString) = iterate(t.s)
+iterate(t::TypstString, state::Integer) = iterate(t.s, state)
+isvalid(t::TypstString, i::Integer) = Base.isvalid(t.s, i)
+
+export TypstString
 
 end # module Typst
