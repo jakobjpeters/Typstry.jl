8d74c94c42a52a11dceb0c3e97885a4b643e92e8
Jakob Peters <jakobj.peters@gmail.com>
2023-08-07 22:14:48 -0700

    Enable `doctest`s

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,19 @@
 
 using Test: @testset, @test, detect_ambiguities, detect_unbound_args
+using Documenter: DocMeta.setdocmeta!, doctest
 using Typstry
 
-@testset "`detect_ambiguities` and `detect_unbound_args`" begin
-    for detect in (:detect_ambiguities, :detect_unbound_args)
-        @eval @test isempty($detect(Typstry, recursive = true))
-    end
+@testset "`detect_ambiguities` and `detect_unbound_args`" for detect in (
+    :detect_ambiguities, :detect_unbound_args
+)
+    @eval @test isempty($detect(Typstry, recursive = true))
 end
+
+setdocmeta!(
+    Typstry,
+    :DocTestSetup,
+    :(using Typstry),
+    recursive = true
+)
+
+@testset "`doctest`" doctest(Typstry)
