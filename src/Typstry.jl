4f7261381a75b1f8d73792a068a5048b420c5147
Jakob Peters <jakobj.peters@gmail.com>
2023-10-30 12:58:12 -0700

    Improve interface and documentation
    Implement `@typst_cmd` and `render`.

--- src/Typstry.jl
+++ src/Typstry.jl
@@ -2,119 +2,216 @@
 module Typstry
 
 import Typst_jll
+using Base: Meta.parse
+
+# Internals
 
 """
-    typst(args...)
-
-Call the `typst` command-line interface with the given arguments via
-[`Typst_jll.jl`](https://github.com/JuliaBinaryWrappers/Typst_jll.jl).
-
-Equivalent to `\$ typst args...` in the shell.
+    interpolate(xs, s, start, stop)
 """
-typst(args...) = Typst_jll.typst(exe -> run(Cmd([exe, args...])))
+interpolate(xs, s, start, stop) = push!(xs, esc(parse(s[start:stop])))
 
 """
-    typst(command::Function, args...; pre_options = (), post_options = ())
-
-Equivalent to `typst(pre_options..., string(command), post_options..., args...)`.
+    typst_cmd(s)
 """
-typst(command::Function, args...; pre_options = (), post_options = ()) =
-    typst(pre_options..., string(command), post_options..., args...)
+typst_cmd(s) = Typst_jll.typst(exe -> Cmd([exe, map(string, split(s))...]))
 
-"""
-    compile(input, [output]; pre_options = (), post_options = ())
-
-Equivalent to [`typst(compile, input, output; pre_options, post_options)`](@ref typst).
-```
-"""
-compile(input, output = nothing; pre_options = (), post_options = ()) =
-    typst(compile, filter(!isnothing, (input, output))...; pre_options, post_options)
+# Interface
 
 """
-    watch(input, [output]; pre_options = (), post_options = ())
+    @typst_str(s)
 
-Equivalent to [`typst(watch, input, output; pre_options, post_options)`](@ref typst).
-"""
-watch(input, output = nothing; pre_options = (), post_options = ()) =
-    typst(watch, filter(!isnothing, (input, output))...; pre_options, post_options)
+Construct a string with custom interpolation and without unescaping.
+Backslashes (`\\`) and quotation marks (`\"`) must still be escaped.
 
-"""
-    fonts(; pre_options = (), post_options = ())
-
-Equivalent to [`typst(fonts; pre_options, post_options)`](@ref typst).
-"""
-fonts(; pre_options = (), post_options = ()) = typst(fonts; pre_options, post_options)
-
-"""
-    help([command::Function])
-
-Prints the help message for `typst` or the given `command`.
-
-Equivalent to [`typst(help, command)`](@ref typst).
-
-# Examples
-```jldoctest
-julia> help();
-typst creates PDF files from .typ files
-
-Usage: typst [OPTIONS] <COMMAND>
-
-Commands:
-  compile  Compiles the input file into a PDF file [aliases: c]
-  watch    Watches the input file and recompiles on changes [aliases: w]
-  fonts    List all discovered fonts in system and custom font paths
-  help     Print this message or the help of the given subcommand(s)
-
-Options:
-      --font-path <DIR>  Add additional directories to search for fonts [env: TYPST_FONT_PATHS=]
-      --root <DIR>       Configure the root for absolute paths [env: TYPST_ROOT=]
-  -v, --verbosity...     Sets the level of verbosity: 0 = none, 1 = warning & error, 2 = info, 3 = debug, 4 = trace
-  -h, --help             Print help
-  -V, --version          Print version
-
-julia> help(fonts);
-List all discovered fonts in system and custom font paths
-
-Usage: typst fonts [OPTIONS]
-
-Options:
-      --variants  Also list style variants of each font family
-  -h, --help      Print help
-```
-"""
-help() = typst(help)
-help(command::Function) = typst(help, string(command))
-
-"""
-    typst_str(s)
-
-Construct a string with custom interpolation and without unescaping
-(except for quotation marks, `\"`, which still have to be escaped).
-
-The `\$` symbol cannot be used to interpolate Julia values,
-because `Typst` uses that symbol to start and end a math mode block.
-Instead, use `\$\$` with the same semantics.
+Use `\$\$` to interpolate values.
+The `\$` symbol is not used because Typst uses
+that symbol to start and end a math mode block.
 
 # Examples
 ```jldoctest
 julia> x = 1;
 
-julia> typst"\$1 / x\$" # math mode
+julia> typst"\$1 / x\$"
 \"\\\$1 / x\\\$\"
 
-julia> typst"\$\$x \$\$(x + 1)" # interpolation
+julia> typst"\$\$x \$\$(x + 1)"
 "1 2"
 
-julia> typst"\\\$\$x" # escaping
+julia> typst"\\\$\$x"
 "\\\$\\\$x"
+
+julia> typst"\\\\"
+"\\\\"
 ```
 """
 macro typst_str(s)
-    esc(Meta.parse("\"" * replace(s,
-        "\\\$\$" => "\\\$\\\$", "\$\$" => "\$", "\$" => "\\\$", "\\" => "\\\\"
-    ) * "\""))
+    i, n, xs = 1, length(s), []
+    while i <= n
+        maybe_j = findnext("\$\$", s, i)
+        if isnothing(maybe_j)
+            push!(xs, s[i:n])
+            break
+        end
+        j = first(maybe_j) - 1
+        push!(xs, s[i:j - 1])
+        i = j + 3
+        if j != 0 && s[j] == '\\' push!(xs, s[j + 1: j + 2])
+        else
+            (j + 2 == n || s[j + 3] == ' ') && error("invalid interpolation syntax: missing value")
+            j != 0 && push!(xs, s[j])
+            k = j + 3
+            if s[i] == '('
+                i += open = 1
+                while open != 0 && (i <= n || error("invalid interpolation syntax: unmatched parentheses"))
+                    open += (s[i] == '(') - (s[i] == ')')
+                    i += 1
+                end
+                interpolate(xs, s, k, i - 1)
+            else
+                maybe_i = findnext(' ', s, i)
+                if isnothing(maybe_i)
+                    interpolate(xs, s, k, n)
+                    break
+                end
+                i = only(maybe_i)
+                interpolate(xs, s, k, i)
+            end
+        end
+    end
+    :(string($(xs...)))
 end
 
-export typst, compile, watch, fonts, help, @typst_str
+"`\$x`"
+
+"""
+    @typst_cmd(s)
+
+Return a `Cmd` such that ```run(typst`\$s`)```
+is equivalent to `shell> typst \$s`.
+
+!!! note
+    This macro does not yet support interpolation.
+
+# Examples
+```jldoctest
+julia> run(typst`help`);
+The Typst compiler
+
+Usage: typst [OPTIONS] <COMMAND>
+
+Commands:
+  compile  Compiles an input file into a supported output format [aliases: c]
+  watch    Watches an input file and recompiles on changes [aliases: w]
+  query    Processes an input file to extract provided metadata
+  fonts    Lists all discovered fonts in system and custom font paths
+  update   Self update the Typst CLI (disabled)
+  help     Print this message or the help of the given subcommand(s)
+
+Options:
+  -v, --verbosity...  Sets the level of logging verbosity: -v = warning & error, -vv = info, -vvv = debug, -vvvv = trace
+      --cert <CERT>   Path to a custom CA certificate to use when making network requests [env: TYPST_CERT=]
+  -h, --help          Print help
+  -V, --version       Print version
+
+julia> write("input.typ", typst"\$x ^ 2\$");
+
+julia> run(typst`compile input.typ output.pdf`);
+```
+"""
+macro typst_cmd(s)
+    typst_cmd(s)
+end
+
+"""
+    typst(s)
+
+Equivalent to `shell> typst \$s`.
+
+# Examples
+```jldoctest
+julia> typst("help");
+The Typst compiler
+
+Usage: typst [OPTIONS] <COMMAND>
+
+Commands:
+  compile  Compiles an input file into a supported output format [aliases: c]
+  watch    Watches an input file and recompiles on changes [aliases: w]
+  query    Processes an input file to extract provided metadata
+  fonts    Lists all discovered fonts in system and custom font paths
+  update   Self update the Typst CLI (disabled)
+  help     Print this message or the help of the given subcommand(s)
+
+Options:
+  -v, --verbosity...  Sets the level of logging verbosity: -v = warning & error, -vv = info, -vvv = debug, -vvvv = trace
+      --cert <CERT>   Path to a custom CA certificate to use when making network requests [env: TYPST_CERT=]
+  -h, --help          Print help
+  -V, --version       Print version
+
+julia> write("input.typ", typst"\$x ^ 2\$");
+
+julia> typst("compile input.typ output.pdf");
+```
+"""
+typst(s) = run(typst_cmd(s))
+
+"""
+    typst(xs...)
+
+Equivalent to `typst(join(xs, " "))`.
+
+# Examples
+```jldoctest
+julia> write("input.typ", typst"\$x ^ 2\$");
+
+julia> typst("compile", "input.typ", "output.pdf");
+```
+"""
+typst(xs...) = typst(join(xs, " "))
+
+"""
+    render(::String; input = "input.typ", output = "output.pdf", open = true)
+
+Render the given string to a document.
+
+This function generates two files.
+The first is the `input`, which contains the Typst document.
+The second is the `output`, which is rendered from the `input` using Typst's compile function.
+
+The document format is inferred by the file extension of `output`.
+The available formats are `pdf`, `png`, and `svg`.
+
+If `open`, `output` will be opened using the default viewer.
+
+!!! note
+    This is designed to generate a document with little effort.
+    For more advanced useage, see [`typst`](@ref).
+
+# Examples
+```jldoctest
+julia> render(typst"\$x ^ 2\$");
+
+julia> render([1, 2, 3, 4]);
+```
+"""
+function render(document::String; input = "input.typ", output = "output.pdf", open = true)
+    write(input, document)
+    typst(("compile", input, output, "--open")[begin:end - !open]...)
+end
+
+"""
+    render(elements...; delimeter = " \\\\n", kwargs...)
+
+Equivalent to `render(join(xs, delimeter; kwargs...))`.
+
+# Examples
+```jldoctest
+julia> render("The area of a circle is", typst"\$A = pi dot r ^ 2\$"; delimeter = " ");
+```
+"""
+render(elements...; delimeter = " \\\n", kwargs...) = render(join(elements, delimeter); kwargs...)
+
+export @typst_str, @typst_cmd, typst, render
 
 end # module
