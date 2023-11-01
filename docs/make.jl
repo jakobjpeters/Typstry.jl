a1081e389b8e092d4060f2cbc80f6ade49d9002a
Jakob Peters <jakobj.peters@gmail.com>
2023-11-01 16:33:45 -0700

    Move `logo.jl` into `make.jl`

--- docs/make.jl
+++ docs/make.jl
@@ -2,10 +2,25 @@
 using Documenter: DocMeta.setdocmeta!, makedocs, HTML, deploydocs
 using Typstry
 
-const directory = (@__DIR__) * "/src/assets/"
-if !ispath(directory * "logo.svg")
-    include("logo.jl")
-    make_logo(directory)
+const directory = joinpath(@__DIR__, "src", "assets")
+const logo = joinpath(directory, "logo.svg")
+if !ispath(logo)
+    using Luxor: Drawing, julia_blue, julia_red, julia_green, julia_purple, sethue, rect, finish
+
+    const width, height = 210, 297
+
+    mkpath(directory)
+    Drawing(width, height, :svg, logo)
+
+    for (color, (x_min, y_min)) in zip(
+        (julia_purple, julia_green, julia_red, julia_blue),
+        map(i -> ((0.3 - i) * width, i * height), 0:0.1:0.3)
+    )
+        sethue(color)
+        rect(x_min, y_min, 0.7 * width, 0.7 * height; action = :fill)
+    end
+
+    finish()
 end
 
 setdocmeta!(
@@ -19,10 +34,7 @@ makedocs(
     sitename = "Typstry.jl",
     format = HTML(edit_link = "main"),
     modules = [Typstry],
-    pages = [
-        "Home" => "index.md",
-        "Manual" => "manual.md"
-    ],
+    pages = ["Home" => "index.md", "Manual" => "manual.md"],
 )
 
 deploydocs(
