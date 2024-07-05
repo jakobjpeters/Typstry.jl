4746f00fe52f9d67a8be374119dc7570b0cd3333
Jakob Peters <jakobj.peters@gmail.com>
2024-07-05 06:15:18 -0700

    Write tests for `Mode`, `Typst`, and `context`

--- test/TestStrings.jl
+++ test/TestStrings.jl
@@ -1,7 +1,56 @@
 
 module TestStrings
 
-test() = nothing
+import Typstry: context
+using Test: @test, @testset
+using Typstry
+
+struct X end
+
+const default_context = Dict(
+    :backticks => 3,
+    :block => false,
+    :depth => 0,
+    :mode => markup,
+    :parenthesize => true,
+    :tab_size => 2
+)
+const typst = Typst(1)
+const x = X()
+const x_context = Dict(:x => 1)
+
+context(::X) = x_context
+
+function test()
+    @testset "`Mode`" begin
+        @test Mode <: Enum
+        @test instances(Mode) == map(Mode, (0, 1, 2)) == (code, markup, math)
+    end
+
+    @testset "`Typst`" begin
+        @test typst == typst
+        @test typst != Typst(1.0)
+        @test typeof(typst) == Typst{Int}
+        @test string(typst) == "Typst{Int64}(1)"
+    end
+
+    @testset "`TypstString`" begin end
+
+    @testset "`TypstText`" begin end
+
+    @testset "`@typst_str`" begin end
+
+    @testset "`context`" begin
+        @test context(1) == Dict{Symbol, Union{}}()
+        @test context(X()) == x_context
+        @test context(typst) == default_context
+        @test context(Typst(x)) == merge(default_context, x_context)
+    end
+
+    @testset "`show_typst`" begin end
+
+    @testset "`Base`" begin end
+end
 
 end # TestStrings
 
