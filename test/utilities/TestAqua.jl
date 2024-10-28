3525925bf4732c59278bbd8251e5200ed72a2ffd
Jakob Peters <jakobj.peters@gmail.com>
2024-10-27 20:47:41 -0700

    Fix Aqua.jl test failure for standard library compatibility bounds

--- test/utilities/TestAqua.jl
+++ test/utilities/TestAqua.jl
@@ -1,10 +1,13 @@
 
 module TestAqua
 
-using Aqua: test_all
+using Aqua: test_all, test_deps_compat
 using Typstry: Typstry
 
 # TODO: test package extensions
-redirect_stdout(() -> test_all(Typstry), devnull)
+redirect_stdout(devnull) do
+    test_all(Typstry; deps_compat = false)
+    test_deps_compat(Typstry; check_weakdeps = (ignore = [:Markdown],), ignore = [:Artifacts, :Dates])
+end
 
 end # TestAqua
