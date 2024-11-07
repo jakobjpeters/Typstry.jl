be8f27357cd1765d7537d6dc262fc1e315955995
Jakob Peters <jakobj.peters@gmail.com>
2024-11-06 18:42:05 -0800

    Create `utilities.jl`

--- src/Typstry.jl
+++ src/Typstry.jl
@@ -1,16 +1,7 @@
 
 module Typstry
 
-using PrecompileTools: @compile_workload
-
-_unwrap(type, key, value::T) where T = value isa type ? value : throw(ContextError(type, T, key))
-
-"""
-    unwrap(io, type, key)
-    unwrap(io, type, key, default)
-"""
-unwrap(io, type, key) = _unwrap(type, key, io[key])
-unwrap(io, type, key, default) = _unwrap(type, key, get(io, key, default))
+include("utilities.jl")
 
 include("Strings.jl")
 using .Strings: ContextError, Mode, Typst, TypstString, TypstText, @typst_str, code, markup, math, context, show_typst
@@ -19,16 +10,6 @@ export ContextError, Mode, Typst, TypstString, TypstText, @typst_str, code, mark
 include("Commands.jl")
 using .Commands: TypstCommand, TypstError, @typst_cmd, julia_mono, preamble, render, set_preamble, typst
 export TypstCommand, TypstError, @typst_cmd, julia_mono, preamble, render, set_preamble, typst
-
-"""
-    compile_workload(examples)
-
-Given an iterable of value-type pairs, interpolate each value into
-a `@typst_str` within a `PrecompileTools.@compile_workload` block.
-"""
-compile_workload(examples) = @compile_workload for (x, _) in examples
-    typst"\(x)"
-end
 
 compile_workload(Strings.examples)
 
---
+++ src/utilities.jl
@@ -0,0 +1,61 @@
+
+using .Iterators: Stateful
+using PrecompileTools: @compile_workload
+
+"""
+    compile_workload(examples)
+
+Given an iterable of value-type pairs, interpolate each value into
+a `@typst_str` within a `PrecompileTools.@compile_workload` block.
+"""
+compile_workload(examples) = @compile_workload for (example, _) in examples
+    TypstString(example)
+end
+
+"""
+    enclose(f, io, x, left, right = reverse(left); kwargs...)
+
+Call `f(io,\u00A0x;\u00A0kwargs...)` between printing `left` and `right`, respectfully.
+
+# Examples
+
+```jldoctest
+julia> Typstry.enclose((io, i; x) -> print(io, i, x), stdout, 1, "\\\$ "; x = "x")
+\$ 1x \$
+```
+"""
+function enclose(f, io, x, left, right = reverse(left); context...)
+    print(io, left)
+    f(io, x; context...)
+    print(io, right)
+end
+
+"""
+    join_with(f, io, xs, delimeter; kwargs...)
+
+Similar to `join`, except printing with `f(io, x; kwargs...)`.
+
+# Examples
+
+```jldoctest
+julia> Typstry.Strings.join_with((io, i; x) -> print(io, -i, x), stdout, 1:4, ", "; x = "x")
+-1x, -2x, -3x, -4x
+```
+"""
+function join_with(f, io, xs, delimeter; kwargs...)
+    _xs = Stateful(xs)
+
+    for x in _xs
+        f(io, x; kwargs...)
+        isempty(_xs) || print(io, delimeter)
+    end
+end
+
+_unwrap(type, key, value::T) where T = value isa type ? value : throw(ContextError(type, T, key))
+
+"""
+    unwrap(io, type, key)
+    unwrap(io, type, key, default)
+"""
+unwrap(io, type, key) = _unwrap(type, key, io[key])
+unwrap(io, type, key, default) = _unwrap(type, key, get(io, key, default))
